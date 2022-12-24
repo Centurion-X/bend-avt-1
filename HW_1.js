24f891c6dbc29fde57a55a858d0987de5019bc1d
Serge Monakhov <58781475+Centurion-X@users.noreply.github.com>
2022-12-24 11:34:54 +0300

    Update HW_1.js
    Similar actions for displaying tasks results are united in one function 'display_results'.

--- HW_1.js
+++ HW_1.js
@@ -10,6 +10,15 @@ var custom_array,
     object_two,
     result;
 
+/* Функция для вывода подтверждения выполнения задачи */
+const display_results = function (id, print = false)
+{
+    if (print) result = print_array(result);
+    element = document.getElementById(id),
+    element.innerText = result;
+    console.log(`Задание ${id}: ${result}`);
+};
+
 /* Функция для строчного вывода содержания массива */
 const print_array = function (array, space) 
 {
@@ -43,22 +52,18 @@ const print_array = function (array, space)
 /* Задание №0 */
 custom_string = 'Я - строка !',
 custom_function = (string) => string.length,
-element = document.getElementById('0'),
 result = custom_function(custom_string);
     
-console.log('Задание 0: ' + result);
-element.innerText = result;
+display_results(0);
 
 /* Задание №1a */
 custom_array = ['Я короткая строка','Я вроде бы тоже короткая строка','А я длинная строка'],
 custom_number = 20,
 custom_function = (array, limit) =>
  array.filter(string => string.length <= limit),
-element = document.getElementById('1a'),
 result = custom_function(custom_array, custom_number);
     
-console.log('Задание 1.1: ' + print_array(result));
-element.innerText = print_array(result);
+display_results('1a', true);
 
 /* Задание №1b */
 custom_function = (array, limit) =>
@@ -66,11 +71,9 @@ custom_function = (array, limit) =>
  for (item = 0; item < array.length; item++)
  {if (array[item].length <= limit) result.push(array[item])}
  return result},
-element = document.getElementById('1b'),
 result = custom_function(custom_array, custom_number);
     
-console.log('Задание 1.2: ' + print_array(result));
-element.innerText = print_array(result);
+display_results('1b', true);
 
 /* Задание №2a */
 custom_array = [{ name: 'shark', likes: 'ocean' },
@@ -78,11 +81,9 @@ custom_array = [{ name: 'shark', likes: 'ocean' },
                 { name: 'otter', likes: 'fish biscuits' }],
 custom_function = (array) =>
  array.map(object => object.name + ' ' + Object.keys(object)[1] + ' ' + object.likes),
-element = document.getElementById('2a'),
 result = custom_function(custom_array);
     
-console.log('Задание 2.1: ' + print_array(result));
-element.innerText = print_array(result);
+display_results('2a', true);
 
 /* Задание №2b */
 custom_string = 'likes',
@@ -92,53 +93,43 @@ custom_function = (array) =>
  {object = array[item];
   result.push(object.name + ' ' + custom_string + ' ' + object.likes)}
  return result},
-element = document.getElementById('2b'),
 result = custom_function(custom_array, custom_string);
     
-console.log('Задание 2.2: ' + print_array(result));
-element.innerText = print_array(result);
+display_results('2b', true);
 
 /* Задание №3a */
 object_one = { name: 'Алиса' },
 object_two = { age: 11 },
 custom_function = (object_one, object_two) =>
  object = {...object_one, ...object_two},
-element = document.getElementById('3a'),
 result = custom_function(object_one, object_two);
     
-console.log('Задание 3.1: ' + print_array(result));
-element.innerText = print_array(result);
+display_results('3a', true);
 
 /* Задание №3b */
 object_one = { name: 'Александра' },
 object_two = { age: 8 },
 custom_function = (object_one, object_two) =>
  Object.assign({}, object_one, object_two),
-element = document.getElementById('3b'),
 result = custom_function(object_one, object_two);
     
-console.log('Задание 3.2: ' + print_array(result));
-element.innerText = print_array(result);
+display_results('3b', true);
 
 /* Задание №4 */
 custom_array = [1,2,3,4],
 custom_function = (array) =>
  Math.min(...array),
-element = document.getElementById('4'),
 result = custom_function(custom_array);
     
-console.log('Задание 4: ' + result);
-element.innerText = result;
+display_results(4);
 
 /* Задание №5 */
 custom_array = [1,2,3,4],
 custom_function = (array) =>
  array.reduce((list, value) => {value % 2 != 0 && list.push(value); return list}, []),
-element = document.getElementById('5'),
 result = custom_function(custom_array);
     
-console.log('Задание 5: ' + print_array(result));
-element.innerText = print_array(result);
+display_results(5, true);
 
 /* Задание №6 */
 custom_array = [{ price: 10, count: 2 },
@@ -147,21 +138,17 @@ custom_array = [{ price: 10, count: 2 },
                 { price: 15, count: 6 }],
 custom_function = (array) =>
  array.reduce((sum, item) => sum + item.count * item.price, 0),
-element = document.getElementById('6'),
 result = custom_function(custom_array);
     
-console.log('Задание 6: ' + result);
-element.innerText = result;
+display_results(6);
 
 /* Задание №7 */
 custom_array = [1, 2, 2, 4, 5, 5],
 custom_function = (array) =>
  array.reduce((set, value) => {set.indexOf(value) == -1 && set.push(value); return set}, []),
-element = document.getElementById('7'),
 result = custom_function(custom_array);
     
-console.log('Задание 7: ' + print_array(result));
-element.innerText = print_array(result);
+display_results(7, true);
 
 /* Задание №8 */
 object = {'401': 'Ошибка авторизации',
@@ -191,21 +178,17 @@ custom_function = (error) =>
     };
     return error;
 },
-element = document.getElementById('8'),
 result = custom_function('abracadabra');
     
-console.log('Задание 8: ' + result);
-element.innerText = result;
+display_results(8);
 
 /* Задание №9 */
 custom_array = [4,3,2,1],
 custom_function = (array) =>
  array.sort((a, b) => a - b).slice(0, 2),
-element = document.getElementById('9'),
 result = custom_function(custom_array);
     
-console.log('Задание 9: ' + print_array(result));
-element.innerText = print_array(result);
+display_results(9, true);
 
 /* Задание №10 */
 custom_array = {firstName: 'Петр',
@@ -213,22 +196,18 @@ custom_array = {firstName: 'Петр',
                 patronymic: 'Иванович'},
 custom_function = (array) =>
  `ФИО => ${[].concat(array.firstName, array.patronymic, array.secondName).join(' ')}`,
-element = document.getElementById('10'),
 result = custom_function(custom_array);
     
-console.log('Задание 10: ' + result);
-element.innerText = result;
+display_results(10);
 
 /* Задание №11 */
 custom_array = [1,2,3,4],
 custom_number = 5,
 custom_function = (array, multiplier) =>
  array.map(number => number * multiplier),
-element = document.getElementById('11'),
 result = custom_function(custom_array, custom_number);
     
-console.log('Задание 11: ' + print_array(result));
-element.innerText = print_array(result);
+display_results(11, true);
 
 /* Задание №12 */
 custom_array = [{name: 'Batman', franchise: 'DC'},
@@ -238,8 +217,6 @@ custom_array = [{name: 'Batman', franchise: 'DC'},
 custom_function = (array, franchise) =>
  franchise + ' => ' +
  array.filter(string => string.franchise == franchise).map(string => string.name).join(', '),
-element = document.getElementById('12'),
 result = custom_function(custom_array, 'Marvel');
     
-console.log('Задание 12: ' + result);
-element.innerText = result;
+display_results(12);
